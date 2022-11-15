f8242640aa4423e3b3b5ac86396b32a2c16fdae7
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-15 15:09:11 +0100

    adding assets to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 /** @format */
 
 import './App.css';
-import './components/Navbar';
+
+import assets from './assets';
 import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<h1 className="display-1">Hello App</h1>
+			<h1 className="display-1">{assets.dogs[0].name}</h1>
 		</div>
 	);
 }
